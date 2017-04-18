f0e9e2a5b2192c263fe15af5abbaef3d32e28833
Ricards <G00314715@gmit.ie>
2017-04-18 19:32:44 +0100

    9. Score, Enemy Shoot & Collision Detection

--- static/resources/player.js
+++ static/resources/player.js
@@ -4,6 +4,10 @@ var canvas = document.getElementById("player-canvas");
 var context = canvas.getContext("2d");
 
 var shotFired = true;
+var score = 0;
+
+var percentFire = .01;
+var chance = 0;
 
 function player (x,y,width,height,speed){
 	this.x =x;
@@ -12,8 +16,7 @@ function player (x,y,width,height,speed){
 	this.height = height;
 	this.speed = speed;
 	
-	//+10 to (x,y) because the center of a rectangle is the upper left corner
-	playerBullet = new bullet(this.x + 10,this.y, 10);
+	playerBullet = new bullet(0, 0, 10);
 	
 	this.draw = function(){
 		context.beginPath();
@@ -35,26 +38,26 @@ function player (x,y,width,height,speed){
 				this.x -= this.speed
 				if (this.x <= 10){ //aprox 10px from the left canvas edge
 					this.x = 10;
-				}
-			} 	
+				}//inner if
+			}//if
 			if (KEY_STATUS.right) {
 				this.x += this.speed
 				if (this.x >= canvas.width - 30){ //approx 10px from the right canvas edge
 					this.x = canvas.width - 30;
-				}
-			} 	
+				}//inner if
+			}//if	
 			if (KEY_STATUS.up) {
 				this.y -= this.speed
 				if (this.y <= 800){ //only allow the object to move up to (x,800)
 					this.y = 800;
-				}
-			} 	
+				}//inner if
+			}//if	
 			if (KEY_STATUS.down) {
 				this.y += this.speed
 				if (this.y >= canvas.height - 130){ //approx 10px from the bottom canvas edge
 					this.y = canvas.height - 130;
-				}
-			}
+				}//inner if
+			}//if
 		}//if
 		playerObject.draw();
 		//if space bar is pressed, set shotFired to true (triggers playerBullet.move inside init function) and if the bullet is off the canvas set it to player's location
@@ -68,7 +71,7 @@ function player (x,y,width,height,speed){
 	}//move
 }//player
 
-//bullet object for player and enemies
+//bullet object for player and enemy bosses
 function bullet(x,y,r){
 	this.x = x;
 	this.y = y;
@@ -95,31 +98,75 @@ function init(){
 	context.clearRect(0, 0, canvas.width, canvas.height);
 	playerObject.move();
 
-	//only move and checkCollision if the object has not been hit by the player's bullet
+	//only execute these functions if the object has not been hit by the player's bullet
 	for (var i =0; i < 16; i++){
 		if (enemiesMid[i].hit == 0){
 			enemiesMid[i].move();
+			enemiesMid[i].bulletY += 5;
 			enemiesMid[i].checkCollision();
+			enemiesMid[i].bulletMove();
+			enemiesMid[i].bulletCollision();
 		}//if
 	}//for
 	
+	//execute these functions if the enemy is hit and has also fired a shot
+	//without this loop the enemy bullet would disappear along with the hit enemy
+	//only execute these functions if the bullet is in inside the canvas (bulletY < canvas.height) otherwise the bullet remains at 1080 y where it can do no harm
+	for (i = 0; i < 16; i++){
+		if(enemiesMid[i].hit == 1 && enemiesMid[i].bulletFired == 1){
+			if(enemiesMid[i].bulletY < canvas.height){
+				enemiesMid[i].bulletMove();
+				enemiesMid[i].bulletY += 5;
+				enemiesMid[i].bulletCollision();
+			}//inner if
+		}//if
+	}//for
+	
+	//left enemies same idea as mid except all the bullet functions are inherited from abstract enemy
 	for (i = 0; i < 8; i++){
 		if (enemiesLeft[i].hit == 0){
 			enemiesLeft[i].move();
 			enemiesLeft[i].checkCollision();
+			enemiesLeft[i].bulletMove();
+			enemiesLeft[i].bulletCollision();
 		}//if
 	}//for
 	
 	for (i = 0; i < 8; i++){
+		if(enemiesLeft[i].hit == 1 && enemiesLeft[i].bulletFired == 1){
+			if(enemiesLeft[i].bulletY < canvas.height){
+				enemiesLeft[i].bulletMove();
+				enemiesLeft[i].bulletCollision();
+			}//inner if
+		}//if
+	}//for
+	
+	//right enemies same idea as mid except all the bullet functions are inherited from abstract enemy
+	for (i = 0; i < 8; i++){
 		if(enemiesRight[i].hit == 0){
 			enemiesRight[i].move();
 			enemiesRight[i].checkCollision();
+			enemiesRight[i].bulletMove();
+			enemiesRight[i].bulletCollision();
 		}//if
 	}//for
+	
+	for (i = 0; i < 8; i++){
+		if(enemiesRight[i].hit == 1 && enemiesRight[i].bulletFired == 1){
+			if(enemiesRight[i].bulletY < canvas.height){
+				enemiesRight[i].bulletMove();
+				enemiesRight[i].bulletCollision();
+			}//inner if
+		}//if
+	}//for
+	
 	
 	if (shotFired == true){
 		playerBullet.move();
 	}//if
+	
+	//draw the score on the canvas
+	drawScore();
 	
 	window.requestAnimationFrame(init);
 }//init
@@ -172,11 +219,68 @@ document.onkeyup = function(e) {
 function enemy(){
 	this.spawn = function(){}
 	this.move = function(){}
-	this.shoot = function(){console.log("shoot")}
+	
+	this.checkCollision = function(){
+		//vertical and horizontal distance between the circle's center and the rectangle's center
+		//this is for mid and left because they are rectangles and the playerBullet is a circle
+		var distX = Math.abs(playerBullet.x - this.x-this.width/2);
+		var distY = Math.abs(playerBullet.y - this.y-this.height/2); 
+		
+		//if the distance is less than half rectangle then they are colliding
+		if ((distX <= (this.width/2)) && (distY <= (this.height/2))){
+			this.hit = 1;
+			shotFired = false;
+			playerBullet.y = -500;
+			score = score + 100;
+		}//if
+	}//checkCollision
+	
+	//draw the bullet
+	this.bulletSpawn = function(){
+		context.beginPath();
+		context.arc(this.bulletX, this.bulletY, 5, 0, 2 * Math.PI);
+		context.fillStyle = "DeepPink";
+		context.fill();
+		context.closePath();
+	}//bulletSpawn
+	
+	//everytime this function is executed theres a chance this.bulletFired gets set to 1 unless its already set to 1
+	//once this.bulletFired is set to 1 draw the bullet as it moves across the canvas until it goes off the canvas, then set bulletFired back to 0
+	//same for left and right, different function for mid because they shoot at the same time
+	this.bulletMove = function(){
+		chance = Math.floor(Math.random()*101);
+		if (this.bulletFired == 0 && (chance/100 < percentFire)){
+			this.bulletY = this.y + 25; //its necessary to set bulletX and bulletY here because
+			this.bulletX = this.x + 10; //without them the first bullet that spawns would spawn at the passed bulletX and bulletY
+			this.bulletSpawn();
+			this.bulletFired = 1;
+		}//if
+		
+		if (this.bulletFired == 1){
+			this.bulletY += 5;
+			this.bulletSpawn();
+			if (this.bulletY > canvas.height){
+				//set this.bulletFired to 0 so that it is not spawned right away - repeat the chance calculation
+				this.bulletFired = 0;
+			}//inner if
+		}//if
+	}//bulletMove
+		
+	//collision detection for an enemy bullet hitting the player object
+	this.bulletCollision = function(){
+		//vertical and horizontal distance between the circle's center(bullet) and the rectangle's center(player)
+		var distX = Math.abs(this.bulletX - playerObject.x - playerObject.width/2);
+		var distY = Math.abs(this.bulletY - playerObject.y - playerObject.height/2);
+		
+		//if the distance is less than half rectangle then they are colliding
+		if ((distX <= (playerObject.width/2)) && (distY <= (playerObject.height/2))){
+			console.log("Player hit!");
+		}//if
+	}//bulletCollision
 }//enemy
 
 //enemies mid
-function mid(x, y, dx, dy, width, height, left, right, hit){
+function mid(x, y, dx, dy, width, height, left, right, hit, bulletX, bulletY, bulletFired){
 	this.x = x;
 	this.y = y;
 	this.dx = dx;
@@ -186,6 +290,9 @@ function mid(x, y, dx, dy, width, height, left, right, hit){
 	this.left = left;
 	this.right = right;
 	this.hit = hit;
+	this.bulletX = bulletX;
+	this.bulletY = bulletY;
+	this.bulletFired = bulletFired;
 	
 	this.spawn = function(){
 		context.beginPath();
@@ -212,23 +319,29 @@ function mid(x, y, dx, dy, width, height, left, right, hit){
 		}//if
 	}//move
 	
-	this.checkCollision = function(){
-		//vertical and horizontal distance between the circle's center and the rectangle's center
-		var distX = Math.abs(playerBullet.x - this.x-this.width/2);
-		var distY = Math.abs(playerBullet.y - this.y-this.height/2); 
+	//overwriting enemy move function - these enemies fire at the same time
+	this.bulletMove = function(){
 		
-		//if the distance is less than half rectangle then they are colliding
-		if ((distX <= (this.width/2)) && (distY <= (this.height/2))){
-			//console.log("Collision Mid!")
-			this.hit = 1;
-			shotFired = false;
-			playerBullet.y = -500;
+		if (this.bulletFired == 0){
+			this.bulletSpawn();
+			this.bulletFired = 1;
 		}//if
-	}//checkCollision
+		
+		if (this.bulletFired == 1){
+			this.bulletSpawn();
+			//this.bulletY += 5;
+			
+			if (this.bulletY > canvas.height){
+				this.bulletY = this.y + 25;
+				this.bulletX = this.x + 10;
+				this.bulletFired = 0;
+			}//inner if
+		}//if
+	}//bulletMove
 }//mid
 
 //enemies on the left will continuously move to the right until they are off the canvas and then spawn a bit off the canvas on the left and move right until destroyed
-function left(x, y, dx, dy, width, height, hit){
+function left(x, y, dx, dy, width, height, hit, bulletX, bulletY, bulletFired){
 	this.x = x;
 	this.y = y;
 	this.dx = dx;
@@ -236,6 +349,9 @@ function left(x, y, dx, dy, width, height, hit){
 	this.width = width;
 	this.height = height;
 	this.hit = hit;
+	this.bulletX = bulletX;
+	this.bulletY = bulletY;
+	this.bulletFired = bulletFired;
 	
 	this.spawn = function(){
 		context.beginPath();
@@ -253,28 +369,19 @@ function left(x, y, dx, dy, width, height, hit){
 			this.x = -20;
 		}//inner if
 	}//move
-	
-	this.checkCollision = function(){
-		var distX = Math.abs(playerBullet.x - this.x-this.width/2);
-		var distY = Math.abs(playerBullet.y - this.y-this.height/2); 
-		
-		if ((distX <= (this.width/2)) && (distY <= (this.height/2))){
-			//console.log("Collision Left!")
-			this.hit = 1;
-			shotFired = false;
-			playerBullet.y = -500;
-		}//if
-	}//checkCollision
 }//left
 
 //enemies on the right will continuously move to the left until they are off the canvas and then spawn a bit off the canvas on the right and move left until destroyed
-function right(x, y, dx, dy, r, hit){
+function right(x, y, dx, dy, r, hit, bulletX, bulletY, bulletFired){
 	this.x = x;
 	this.y = y;
 	this.dx = dx;
 	this.dy = dy;
 	this.r = r;
 	this.hit = hit;
+	this.bulletX = bulletX;
+	this.bulletY = bulletY;
+	this.bulletFired = bulletFired;
 	
 	this.spawn = function(){
 		context.beginPath();
@@ -293,8 +400,9 @@ function right(x, y, dx, dy, r, hit){
 		}//inner if
 	}//move
 	
+	//overwriting enemy checkCollision function because unlike mid and left this is a circle
 	this.checkCollision = function(){
-		//horizontal and vertical distance between 2 balls
+		//horizontal and vertical distance between 2 circles
 		var colX = playerBullet.x - this.x;
 		var colY = playerBullet.y - this.y;
 		
@@ -307,9 +415,22 @@ function right(x, y, dx, dy, r, hit){
 			this.hit = 1;
 			shotFired = false;
 			playerBullet.y = -500;
+			score = score + 100;
 		}//if
 	}//checkCollision
 }//right
+
+//draw score on canvas
+function drawScore() {
+	context.font = "16px sans-serif";
+	context.fillStyle = "green";
+	context.fillText("SCORE: " + score, 10, 20);
+}//drawScore
+
+//increment score by 1 (window.setInterval(incrScore, 3000);) every 3 seconds
+function incrScore(){
+	score = score + 1;
+}//incrScore
 
 mid.prototype = new enemy();
 left.prototype = new enemy();
@@ -324,8 +445,8 @@ var rightMid = 500;
 var q = 8;
 
 for (var e = 0; e < howManyMid; e++){
-	enemiesMid[e] = new mid(xMid, -50, 2, 2, 20, 20, leftMid, rightMid, 0);
-	enemiesMid[q] = new mid(xMid, 0, 2, 2, 20, 20, leftMid, rightMid, 0);
+	enemiesMid[e] = new mid(xMid, -50, 2, 2, 40, 40, leftMid, rightMid, 0, -50, -50, 0);
+	enemiesMid[q] = new mid(xMid, 0, 2, 2, 40, 40, leftMid, rightMid, 0, -50, -50, 0);
 	xMid = xMid + 50;
 	leftMid = leftMid + 50;
 	rightMid = rightMid - 50;
@@ -337,17 +458,18 @@ enemiesLeft = {};
 xLeft = -400;
 
 for (var el = 0; el < 8; el++){
-enemiesLeft[el] = new left(xLeft, 250, 2, 2, 20, 20, 0);
+enemiesLeft[el] = new left(xLeft, 250, 2, 2, 20, 20, 0, 0, 0, 0);
 xLeft = xLeft + 50;
-}
+}//for
 
 //initializing enemies on the right
 enemiesRight = {}
 xRight = canvas.width + 400;
 
 for (var er = 0; er < 8; er++){
-enemiesRight[er] = new right(xRight, 350, 2, 20, 20, 0);
+enemiesRight[er] = new right(xRight, 350, 2, 20, 20, 0, 0, 0, 0);
 xRight = xRight - 50;
-}
+}//for
 
+window.setInterval(incrScore, 3000);
 init();
